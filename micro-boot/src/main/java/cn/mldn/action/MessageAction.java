82267ceb5d76c52d61b01eee31804ea5f75adf54
mldn <mldnqa@163.com>
2018-07-09 10:10:23 +0800

    thymeleaf数据处理。

--- micro-boot/src/main/java/cn/mldn/action/MessageAction.java
+++ micro-boot/src/main/java/cn/mldn/action/MessageAction.java
@@ -1,8 +1,11 @@
 package cn.mldn.action;
- 
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,21 +15,24 @@ import cn.mldn.vo.Message;
 
 @Controller
 public class MessageAction {
-	@RequestMapping("/msg") 
+	@RequestMapping("/msg")
 	public String msg(Model model) {
-		model.addAttribute("age", 18) ;
-		model.addAttribute("name", "mldn") ;
-		List<Message> allMessages = new ArrayList<Message>() ;
-		for (int x = 0 ; x < 2 ; x ++) {
-			Message msg = new Message() ;
-			msg.setMid(101L);
-			msg.setTitle("高大师今晚要回国 - x" + x);
-			msg.setNote("回国开坛做法，为特朗普招魂，重新引导世界人民走向共同繁荣。");
-			msg.setPrice(982929.99);
-			msg.setPub(new Date());
-			allMessages.add(msg) ;
-		}
-		model.addAttribute("allMsgs", allMessages) ;  
-		return "message" ;  
+		model.addAttribute("age", 18);
+		model.addAttribute("name", "mldn");
+		Message msg = new Message();
+		msg.setMid(1L);
+		msg.setTitle("高大师今晚要回国");
+		msg.setNote("回国开坛做法，为特朗普招魂，重新引导世界人民走向共同繁荣。");
+		msg.setPrice(19.8);
+		msg.setPub(new Date());
+		model.addAttribute("msg", msg);
+		Set<String> set = new HashSet<String>();
+		set.addAll(Arrays.asList("happy", "single", "man"));
+		model.addAttribute("info", set);
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(1, "1th");
+		map.put(2, "2th");
+		model.addAttribute("line", map);
+		return "message";
 	}
 }
