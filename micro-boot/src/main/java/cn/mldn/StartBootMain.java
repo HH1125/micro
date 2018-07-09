00f73e06287271c4c3a21ca06cbe090b263343c9
mldn <mldnqa@163.com>
2018-07-09 11:19:58 +0800

    Spring整合Redis连接。

--- micro-boot/src/main/java/cn/mldn/StartBootMain.java
+++ micro-boot/src/main/java/cn/mldn/StartBootMain.java
@@ -3,7 +3,7 @@ package cn.mldn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-@EnableScheduling
+// @EnableScheduling
 @SpringBootApplication 
 public class StartBootMain{
 	public static void main(String[] args) {
---
+++ micro-boot/src/test/java/cn/mldn/test/TestRedisTemplate.java
@@ -0,0 +1,24 @@
+package cn.mldn.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import cn.mldn.StartBootMain;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration // Spring需要启动容器才可以测试
+@SpringBootTest(classes = StartBootMain.class)
+public class TestRedisTemplate {
+	@Autowired
+	private RedisTemplate<String,String> redisTemplate ;
+	@Test
+	public void testSet() {
+		this.redisTemplate.opsForValue().set("mldn", "java");
+		System.out.println(this.redisTemplate.opsForValue().get("mldn"));
+	}
+}
