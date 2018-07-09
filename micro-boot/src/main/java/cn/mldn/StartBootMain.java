64b33f944076110ab22ab84c1df8820132ce3f37
mldn <mldnqa@163.com>
2018-07-09 14:11:44 +0800

    Actuator监控处理操作。

--- micro-boot/src/main/java/cn/mldn/StartBootMain.java
+++ micro-boot/src/main/java/cn/mldn/StartBootMain.java
@@ -2,8 +2,6 @@ package cn.mldn;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-// @EnableScheduling
 @SpringBootApplication 
 public class StartBootMain{
 	public static void main(String[] args) {
