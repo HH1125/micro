b98923d76b7f52dddcd881e963493999c9782950
mldn <mldnqa@163.com>
2018-07-09 09:44:01 +0800

    Thymeleaf逻辑处理（分支与循环。）

--- micro-boot/src/main/java/cn/mldn/action/MessageAction.java
+++ micro-boot/src/main/java/cn/mldn/action/MessageAction.java
@@ -1,6 +1,8 @@
 package cn.mldn.action;
  
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +14,19 @@ import cn.mldn.vo.Message;
 public class MessageAction {
 	@RequestMapping("/msg") 
 	public String msg(Model model) {
-		Message msg = new Message() ;
-		msg.setMid(101L);
-		msg.setTitle("高大师今晚要回国");
-		msg.setNote("回国开坛做法，为特朗普招魂，重新引导世界人民走向共同繁荣。");
-		msg.setPrice(982929.99);
-		msg.setPub(new Date());
-		model.addAttribute("msg", msg) ; 
+		model.addAttribute("age", 18) ;
+		model.addAttribute("name", "mldn") ;
+		List<Message> allMessages = new ArrayList<Message>() ;
+		for (int x = 0 ; x < 10 ; x ++) {
+			Message msg = new Message() ;
+			msg.setMid(101L);
+			msg.setTitle("高大师今晚要回国 - x" + x);
+			msg.setNote("回国开坛做法，为特朗普招魂，重新引导世界人民走向共同繁荣。");
+			msg.setPrice(982929.99);
+			msg.setPub(new Date());
+			allMessages.add(msg) ;
+		}
+		model.addAttribute("allMsgs", allMessages) ;  
 		return "message" ;  
 	}
 }
