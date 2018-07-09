0eef6ab5413e6f55c8e652fecc84bdf0713e63a7
mldn <mldnqa@163.com>
2018-07-09 11:06:24 +0800

    SpringBoot定时调度。

--- micro-boot/src/main/java/cn/mldn/StartBootMain.java
+++ micro-boot/src/main/java/cn/mldn/StartBootMain.java
@@ -2,8 +2,9 @@ package cn.mldn;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableScheduling
+@SpringBootApplication 
 public class StartBootMain{
 	public static void main(String[] args) {
 		SpringApplication.run(StartBootMain.class, args) ;
